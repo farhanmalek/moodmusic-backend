5b8d850e1ab6e5fbdda3ca10a9f2558030bb21a2
Farhan Malek <malekf2328@gmail.com>
2025-04-13 22:32:19 +1200

    more

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from routes import auth, quiz, playlists
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3000/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Routes
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
