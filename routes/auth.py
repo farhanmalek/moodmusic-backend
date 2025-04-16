2c6fd05f68719bd66fbb3d9e0cff4a9144d9ada2
Farhan Malek <malekf2328@gmail.com>
2025-04-16 21:54:38 +1200

    more2

--- routes/auth.py
+++ routes/auth.py
@@ -56,12 +56,22 @@ def get_current_user(request: Request):
     token = request.cookies.get("access_token")
     if not token:
         raise HTTPException(status_code=401, detail="Not authenticated")
+    
     sp_auth = spotify.get_spotify_client(token)
     user_data = sp_auth.me()
+    
     user = User.get_by_id(user_data["id"])
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return user.to_dict()
+    
+    user_image = user_data["images"][0]["url"] if user_data.get("images") else None
+
+    response = {
+        **user.to_dict(), 
+        "spotifyImageUrl": user_image
+    }
+
+    return response
 
 
 @router.get("/logout")
